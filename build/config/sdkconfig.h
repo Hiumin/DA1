7f7c1697ad6be68c6930d4f5c196b95b3e79f2b7
Hiumin <hieunkm04@gmail.com>
2025-06-12 16:47:43 +0700

    update time format

--- build/config/sdkconfig.h
+++ build/config/sdkconfig.h
@@ -1,6 +1,6 @@
 /*
  * Automatically generated file. DO NOT EDIT.
- * Espressif IoT Development Framework (ESP-IDF) 5.1.2 Configuration Header
+ * Espressif IoT Development Framework (ESP-IDF) 5.5.0 Configuration Header
  */
 #pragma once
 #define CONFIG_SOC_BROWNOUT_RESET_SUPPORTED "Not determined"
